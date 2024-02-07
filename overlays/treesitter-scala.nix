5bc06295c908ac65112cfc32c18d1f47a70d3c5e
kciesielski <kciesielski@users.noreply.github.com>
2024-02-07 09:35:07 +0100

    [scala] Update tree-sitter-scala snapshot

--- overlays/treesitter-scala.nix
+++ overlays/treesitter-scala.nix
@@ -1,11 +1,11 @@
 { pkgs }:
 pkgs.tree-sitter.buildGrammar rec {
   language = "scala";
-  version = "d72cdd40329207af482950cb8a004da10d5a8126";
+  version = "45b5ba0e749a8477a8fd2666f082f352859bdc3c";
   src = pkgs.fetchFromGitHub {
     owner = "tree-sitter";
     repo = "tree-sitter-scala";
     rev = version;
-    hash = "sha256-N0VjibXwcsqZEZrm4kZFT/sB8f6NnjP62QhpcwgzA1w=";
+    hash = "sha256-tH2STGmCaVHDL4fOh8whPmiHbwKu8ZxjS4wDt+qVjjs=";
   };
 }
